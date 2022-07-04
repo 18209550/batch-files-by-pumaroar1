9ade01a9f99298620d5f208fab710bd73267c123
18209550 <103085649+18209550@users.noreply.github.com>
2022-07-04 02:49:20 -0400

    Delete Microsoft Edge.bat

--- Microsoft Edge.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Welcome to the Edge Uninstaller! Made by pumaroar1.
-title Microsoft Edge Uninstaller
-pause
-cd "C:\Program Files (x86)\Microsoft\Edge\Application\99.0.1150.55\Installer"
-setup.exe --uninstall --system-level --verbose-logging --force-uninstall
-echo This may NOT work if you don't run as Administrator.
-pause
